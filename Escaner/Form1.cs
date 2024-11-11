18d3d3ef1bc737fe7e42827cb597f9ded565a713
Raul Mendez <raulmendezpersonal.230@gmail.com>
2024-11-10 23:18:01 -0700

    Agregados comentarios

--- Escaner/Form1.cs
+++ Escaner/Form1.cs
@@ -54,7 +54,7 @@ namespace Escaner
             { '$', (4, 199) },
             { 'λ', (2, 99) }
         };
-        // Funcion para 
+        // Funcion para devolver el indice de columna por caracter en la tabla de transiciones
         public int IndiceCaracter(char caracter)
         {
             if (caracter == '(' || caracter == ')' || caracter == ';') return 0;
@@ -81,17 +81,49 @@ namespace Escaner
             InitializeComponent();
         }
 
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            // Reiniciar variable de indice
+            i = 1;
+
+            // Refrescar resaltados de caja de texto
+            string respaldoTexto = cajaDeTexto.Text;
+            cajaDeTexto.Text = "";
+            cajaDeTexto.Text = respaldoTexto;
+
+            // Reiniciar componentes y cadenas acumuladoras
+            tablaIdentificadores.Rows.Clear();
+            tablaConstantes.Rows.Clear();
+            tablaLexica.Rows.Clear();
+            sbIdentificador.Clear();
+            sbConstante.Clear();
+            identificadores.Clear();
+            constantes.Clear();
+
+            // Llamar a validar el texto
+            ValidarTexto(cajaDeTexto, tablaLexica, tablaIdentificadores, tablaConstantes);
+        }
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            // Reiniciar componentes
+            cajaDeTexto.Text = "";
+            lblError.Text = "100 - Sin errores";
+            tablaConstantes.Rows.Clear();
+            tablaIdentificadores.Rows.Clear();
+            tablaLexica.Rows.Clear();
+        }
+
         private void ValidarTexto(RichTextBox rtb, DataGridView tablaLexica, DataGridView tablaIdentificadores, DataGridView tablaConstantes)
         {
             string cadena = rtb.Text;
             int estado = 0, numeroLinea = 1, estadoAnterior = 0;
-
             lblError.Text = error[100];
 
+            // Recorrer la cadena
             for (int i = 0; i < cadena.Length; i++)
             {
                 char caracter = cadena[i];
-                if (IndiceCaracter(caracter) == -1)
+                if (IndiceCaracter(caracter) == -1) // Detectar cadenas invalidas (fuera del diccionario)
                 {
                     lblError.Text = error[101] + " en linea " + numeroLinea.ToString();
                     ResaltarCaracter(rtb, caracter, Color.Yellow);
@@ -99,55 +131,70 @@ namespace Escaner
                 }
                 else
                 {
+                    // Guardar el estado actual y pasar al siguiente
                     estadoAnterior = estado;
                     estado = TT[estado, IndiceCaracter(caracter)];
 
+                    // Si el nuevo estado indica identificador
                     if (estado == 5)
                     {
+                        // Acumular el caracter en la cadena de identificador
                         sbIdentificador.Append(caracter);
+
+                        // Si el caracter es el ultimo
                         if (i == cadena.Length - 1)
                         {
+                            // Agregar el identificador a la lista de identificadores
                             AgregarToken(ref sbIdentificador, 101, ref identificadores, numeroLinea);
                         }
                     }
-                    else if (estado >= 6)
+                    else if (estado >= 6) // Si el nuevo estado es numerico
                     {
-                        sbConstante.Append(caracter); 
-                        
+                        // Acumular el caracter en la cadena de contante 
+                        sbConstante.Append(caracter);
+
+                        // Si el caracter es el ultimo
                         if (i == cadena.Length - 1)
                         {
+                            // Si el estado anterior fue invalido
                             if (estadoAnterior == 7 || estadoAnterior == 10 || estadoAnterior == 12)
                             {
+                                // Saltar error
                                 lblError.Text = error[102] + " en linea " + numeroLinea.ToString();
                                 ResaltarCadena(cajaDeTexto, sbConstante.ToString(), Color.Yellow);
                                 return;
                             }
+                            // Si no, agregar la constante a la lista de constantes
                             AgregarToken(ref sbConstante, 201, ref constantes, numeroLinea);
                         }
                     }
+                    // Si el token es de caracter unico, hay un salto de linea o el caracter es el ultimo
                     else if (IndiceCaracter(caracter) < 4 || IndiceCaracter(caracter) == 9 || i == cadena.Length - 1)
                     {
+                        // Si el estado anterior fue invalido
                         if (estadoAnterior == 7 || estadoAnterior == 10 || estadoAnterior == 12)
                         {
+                            // Saltar error
                             lblError.Text = error[102] + " en linea " + numeroLinea.ToString();
                             ResaltarCadena(cajaDeTexto, sbConstante.ToString(), Color.Yellow);
                             return;
                         }
-
+                        // Llamar los metodos de agregacion de token dinamico
                         AgregarToken(ref sbConstante, 201, ref constantes, numeroLinea);
                         AgregarToken(ref sbIdentificador, 101, ref identificadores, numeroLinea);
 
+                        // Si el caracter es salto de linea, aumentar el numero de linea
                         if (IndiceCaracter(caracter) == 9) numeroLinea++;
+                        // Si no, agregarlo a la tabla lexica
                         else AgregarRawATablaLexica(numeroLinea, caracter.ToString(), codigoToken[caracter].Item1, codigoToken[caracter].Item2);
                     }
                 }
             }
-
+            // Agregar contantes e identificadores de las listas a las tablas
             foreach (var constante in constantes)
             {
                 AgregarRawATablaConstantes(constante.Token, constante.Codigo, string.Join(", ", constante.Linea));
             }
-
             foreach(var identificador in identificadores)
             {
                 AgregarRawATablaIdentificadores(identificador.Token, identificador.Codigo, string.Join(", ", identificador.Linea));
@@ -159,27 +206,26 @@ namespace Escaner
             return tokens.Any(t => t.Token == token);
         }
 
+        // Metodos para agregar dato a tablas
         private void AgregarRawATablaLexica(int linea, string token, int tipo, int codigo)
         {
             if (token == "") return;
 
             tablaLexica.Rows.Add(i++, linea, token, tipo, codigo);
         }
-
         private void AgregarRawATablaIdentificadores(string token, int codigo, string lineas)
         {
             if (token == "") return;
 
             tablaIdentificadores.Rows.Add(token, codigo, lineas);
         }
-
         private void AgregarRawATablaConstantes(string token, int codigo, string lineas)
         {
             if (token == "") return;
 
             tablaConstantes.Rows.Add(token, codigo, lineas);
         }
-
+        // Metodo resaltar un caracter
         private void ResaltarCaracter(RichTextBox rtb, char caracterAResaltar, Color colorResaltado)
         {
             int posicionOriginal = rtb.SelectionStart;
@@ -196,70 +242,41 @@ namespace Escaner
             rtb.SelectionLength = 0;
             rtb.SelectionBackColor = Color.White;
         }
-
+        // Metodo resaltar cadena
         private void ResaltarCadena(RichTextBox rtb, string cadenaAResaltar, Color colorResaltado)
         {
             int posicionOriginal = rtb.SelectionStart;
             int longitudCadena = cadenaAResaltar.Length;
 
-            // Asegurarse de que la cadena no esté vacía
             if (string.IsNullOrEmpty(cadenaAResaltar) || longitudCadena > rtb.Text.Length)
                 return;
 
-            // Recorrer el texto en busca de la cadena
             int posicionActual = 0;
             while ((posicionActual = rtb.Text.IndexOf(cadenaAResaltar, posicionActual)) != -1)
             {
                 rtb.Select(posicionActual, longitudCadena);
                 rtb.SelectionBackColor = colorResaltado;
-
-                // Avanzar la posición actual para seguir buscando
                 posicionActual += longitudCadena;
             }
-
-            // Restaurar la selección original
             rtb.SelectionStart = posicionOriginal;
             rtb.SelectionLength = 0;
             rtb.SelectionBackColor = Color.White;
         }
-
-        private void btnBorrar_Click(object sender, EventArgs e)
-        {
-            cajaDeTexto.Text = "";
-            lblError.Text = "100 - Sin errores";
-            tablaConstantes.Rows.Clear();
-            tablaIdentificadores.Rows.Clear();
-            tablaLexica.Rows.Clear();
-        }
-
-        private void btnValidar_Click(object sender, EventArgs e)
-        {
-            string respaldoTexto = cajaDeTexto.Text;
-            cajaDeTexto.Text = "";
-            cajaDeTexto.Text = respaldoTexto;
-            i = 1;
-            tablaIdentificadores.Rows.Clear();
-            tablaConstantes.Rows.Clear();
-            tablaLexica.Rows.Clear();
-            sbIdentificador.Clear();
-            sbConstante.Clear();
-            identificadores.Clear();
-            constantes.Clear();
-            ValidarTexto(cajaDeTexto, tablaLexica, tablaIdentificadores, tablaConstantes);
-        }
-
         private void AgregarToken(ref StringBuilder token, int codigoInicial, ref List<TokenDinamico>listaTokens, int numeroLinea)
         {
-            if (token.ToString() == "") return;
+            if (token.ToString() == "") return; // Si el token no esta vacio
 
-            string textoToken = token.ToString();
+            string textoToken = token.ToString(); // Guardar su texto
 
+            // Obtener su codigo (nuevo o existente)
             int codigo = listaTokens.Any(c => c.Token == textoToken)
                     ? listaTokens.FirstOrDefault(c => c.Token == textoToken).Codigo
                     : listaTokens.Count + codigoInicial;
 
+            // Agregar token a la tabla lexica
             AgregarRawATablaLexica(numeroLinea, textoToken, codigoInicial == 201 ? 3 : 1, codigo);
 
+            // Si es nuevo token
             if (!ExisteToken(listaTokens, textoToken))
             {
                 TokenDinamico resultadoToken = new TokenDinamico
@@ -269,11 +286,12 @@ namespace Escaner
                     Codigo = listaTokens.Count + codigoInicial
                 };
                 resultadoToken.Linea.Add(numeroLinea);
-
+                // Agregarlo a la lista de tokens
                 listaTokens.Add(resultadoToken);
             }
-            else
+            else // Si ya existe
             {
+                // Agregarle el nuevo numero de linea
                 listaTokens.FirstOrDefault(c => c.Token == textoToken).Linea.Add(numeroLinea);
             }
             token.Clear();
